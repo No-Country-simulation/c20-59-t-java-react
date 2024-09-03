836d63e9127c5ccf08dd0951bd4633f4ec7b9f75
kelly saldaña <kellysalb@hotmail.com>
2024-09-03 16:32:31 -0500

    organizando page confirma

--- c20-59-t-java-react/src/Pages/Confirmacion/Confirmacion.jsx
+++ c20-59-t-java-react/src/Pages/Confirmacion/Confirmacion.jsx
@@ -1,27 +1,25 @@
 import { Button, Container, Row } from 'react-bootstrap';
 import { FaRegHeart, FaBell, FaCalendarAlt, FaArchive, FaArrowUp, FaHeart } from 'react-icons/fa';
 import BottomNavbar from '../shared/BottomNavbar/BottomNavbar';
-import CitaAsignada from '../../componentes/CitaAsignada/CitaAsignada';
+/* import CitaAsignada from '../../componentes/CitaAsignada/CitaAsignada'; */
 import './ConfirmacionStyles.css';  
 
 const Confirmacion = () => {
-  const queryParams = new URLSearchParams(window.location.search);
+/*   const queryParams = new URLSearchParams(window.location.search);
   const specialty = queryParams.get('specialty');
   const doctor = queryParams.get('doctor');
   const date = queryParams.get('date');
-  const time = queryParams.get('time');
+  const time = queryParams.get('time'); */
 
   return (
     <div className='full-screen-container d-flex flex-column'>
-<<<<<<< HEAD
-      <Container fluid className="d-flex justify-content-center align-items-center vh-100">
+      {/* <Container fluid className="d-flex justify-content-center align-items-center vh-100">
         <div className="text-center p-4 confirmacion-container">
           <FaRegHeart size={50} className="confirmacion-icon" />
           <h1>Cita agendada con éxito</h1>
           <div className='containerCitaAegendada'>
             <CitaAsignada specialty={specialty} doctor={doctor} date={date} time={time} />
-          </div>
-=======
+          </div> */}
       
       <Container fluid className=" d-flex justify-content-center align-items-center vh-100">
         <div className="d-flex flex-column text-center align-items-center p-4">
@@ -35,7 +33,6 @@ const Confirmacion = () => {
               <span><b>Doctor:</b> Dr. Trauma Tologo</span>
             </Row>
           </Row>
->>>>>>> 0626a440ac2d14b18f0bb75a33af193dc2ad63a0
           <Row className="confirmacion-row">
             <Button variant="light" className="confirmacion-button">
               <FaBell /> Confirmación Correo Electrónico
