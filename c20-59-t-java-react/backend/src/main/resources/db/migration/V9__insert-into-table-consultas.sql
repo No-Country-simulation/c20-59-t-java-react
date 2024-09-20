2226ad4ecef7a9890ca6790016892c75a07e8b4b
zotel1 <zotelsigel@gmail.com>
2024-09-20 13:01:39 -0300

    update folder backend

---
+++ c20-59-t-java-react/backend/src/main/resources/db/migration/V9__insert-into-table-consultas.sql
@@ -0,0 +1,22 @@
+DELETE FROM `railway`.`consultas`;
+
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('1', '1', '2024-07-05 09:00:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('2', '2', '2024-07-10 11:30:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('3', '3', '2024-07-15 14:00:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('4', '4', '2024-08-01 10:00:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('5', '5', '2024-08-12 15:30:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('6', '6', '2024-08-20 13:45:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('7', '7', '2024-07-08 12:00:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('8', '8', '2024-07-22 16:00:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('9', '9', '2024-08-05 09:15:00', NULL);
+INSERT INTO railway.consultas (medico_id, paciente_id, fecha, motivo_cancelamiento)
+VALUES ('10', '10', '2024-08-25 11:30:00', NULL);
