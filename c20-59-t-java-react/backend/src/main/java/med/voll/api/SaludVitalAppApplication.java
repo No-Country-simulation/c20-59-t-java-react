03980729573184ff0782d751b9fe0398aec6ea07
juanparove <juanparove@gmail.com>
2024-09-19 23:00:59 -0300

    subida de la api sin auth

---
+++ c20-59-t-java-react/backend/src/main/java/med/voll/api/SaludVitalAppApplication.java
@@ -0,0 +1,18 @@
+package med.voll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SaludVitalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludVitalAppApplication.class, args);
+	}
+
+
+}
